433e3ec68f203cfc2ba1dd0a363a9fb3db55ff60
Malkit Singh <malkitsingh32@gmail.com>
2026-02-28 22:14:40 +0530

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient({
+      baseUrl: environment.backendUrl
+    })
   ]
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  backendUrl: 'https://staging-backend-url.com'
+};
